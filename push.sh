f4f5cc221c77df6cc365a7ef5faab9935c12df0d
mariobob <regmibatshal@gmail.com>
2019-11-04 20:04:01 -0500

    add travis

--- push.sh
+++ push.sh
@@ -6,14 +6,14 @@ setup_git() {
 }
 
 commit_website_files() {
-  git checkout -b gh-pages
+  git checkout -b master
   git add . *.html
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
   git remote add origin-pages https://${GH_TOKEN}@github.com/mariobob/mariobob.github.io.git > /dev/null 2>&1
-  git push --quiet --set-upstream origin master
+  git push --quiet --set-upstream origin-pages master
 }
 
 setup_git
